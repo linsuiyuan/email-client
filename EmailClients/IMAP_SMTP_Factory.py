9415c52ba43c5bf679eb8e96573045e914ccc1c2
linsuiyuan <linsuiyuan@163.com>
2024-08-26 11:58:24 +0800

    添加注释文档，添加README.md文件

--- EmailClients/IMAP_SMTP_Factory.py
+++ EmailClients/IMAP_SMTP_Factory.py
@@ -15,6 +15,17 @@ class IMAP_SMTP_Factory:
 
     @staticmethod
     def create_imap(username) -> IMAP4_SSL:
+        """根据邮件地址后缀，创建相应的IMAP4_SSL
+
+        Args:
+            username (str): 邮件地址
+
+        Raises:
+            Exception: 如果是未知的邮件地址后缀，则抛出异常
+
+        Returns:
+            IMAP4_SSL: IMAP4_SSL
+        """
         domain = username.split('@')[-1]
         if domain not in IMAP_SMTP_Factory.domain_to_host:
             raise Exception(f'该邮箱未设置服务器：{username}')
@@ -28,6 +39,17 @@ class IMAP_SMTP_Factory:
     
     @staticmethod
     def create_smtp(username) -> SMTP:
+        """根据邮件地址后缀，创建相应的SMTP
+
+        Args:
+            username (str): 邮件地址
+
+        Raises:
+            Exception: 如果是未知邮件地址后缀，则抛出异常
+
+        Returns:
+            SMTP: SMTP
+        """
         domain = username.split('@')[-1]
         if domain not in IMAP_SMTP_Factory.domain_to_host:
             raise Exception(f'该邮箱未设置服务器：{username}')
--- EmailClients/Netease_IMAP4_SSL.py
+++ EmailClients/Netease_IMAP4_SSL.py
@@ -4,6 +4,15 @@ from imaplib import IMAP4_SSL
 class Netease_IMAP4_SSL(IMAP4_SSL):
 
     def login(self, user, password):
+        """重写父类 login，根据163邮件的特殊需求进行相应的配置
+
+        Args:
+            user (str): 邮件地址
+            password (str): 邮件密码或秘钥
+
+        Returns:
+            _type_: 登录信息和数据
+        """
         typ, dat = super().login(user, password)
         imaplib.Commands["ID"] = ('AUTH',)
         args = ("name", user, "contact", user, "version", "1.0.0", "vendor", "myclient")
